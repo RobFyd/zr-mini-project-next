0d8d81e0f49a0b02cdd8824148b4a74bcc364334
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-31 00:41:04 +0000

    refactor: enhance MainMenu to highlight active path based on navigation

--- front-end/src/app/components/MainMenu/MainMenu.jsx
+++ front-end/src/app/components/MainMenu/MainMenu.jsx
@@ -1,14 +1,21 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./MainMenu.module.css";
 import { GENDERS } from "@/app/constants/productCategories";
 
 export function MainMenu() {
-    return (
-        <ul className={styles.mainMenu}>
-            {GENDERS.map(({ path, categoryName }) => (
-                <li key={path}>
-                    <a href={path}>{categoryName}</a>
-                </li>
-            ))}
-        </ul>
-    );
+  const pathname = usePathname();
+  console.log(pathname);
+
+  return (
+    <ul className={styles.mainMenu}>
+      {GENDERS.map(({ path, categoryName }) => (
+        <li key={path}>
+          <a className={pathname === path ? "active" : ""} href={path}>
+            {categoryName}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
