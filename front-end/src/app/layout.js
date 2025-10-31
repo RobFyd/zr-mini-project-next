9002eaba109804d604d4153c32912453ec6ccef7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-31 00:14:34 +0000

    refactor: improve layout structure and add metadata export

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,9 +1,14 @@
 import globals from "./globals.css";
 
+export const metadata = {
+  title: "TOPSHOP",
+  description: "The best shop for trendy clothes in Next.js.",
+};
+
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
