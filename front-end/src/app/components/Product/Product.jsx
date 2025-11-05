4b1495bdfbac1f113e3808d3cbc2b9f69329a5fd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-11-05 00:18:27 +0000

    feat: use CurrencyContext in Product component for dynamic currency display

--- front-end/src/app/components/Product/Product.jsx
+++ front-end/src/app/components/Product/Product.jsx
@@ -1,29 +1,31 @@
+"use client";
+
+import { useContext } from "react";
 import { CURRENCIES, CURRENCY_SIGN } from "../../constants/currencies";
 import styles from "./Product.module.css";
+import { CurrencyContext } from "@/app/contexts/CurrencyContext";
 
 export function Product({ product }) {
-    const currency = CURRENCIES.PLN;
-    return (
-        <>
-            <div className={styles.productCard}>
-                <img src={product.photos[0]} />
+  const [currency] = useContext(CurrencyContext);
+  return (
+    <>
+      <div className={styles.productCard}>
+        <img src={product.photos[0]} />
 
-                <button>
-                    <div className={styles.favourite}></div>
-                </button>
+        <button>
+          <div className={styles.favourite}></div>
+        </button>
 
-                <div>
-                    <p className={styles.name}>{product.productName}</p>
-                    <p className={styles.productPrice}>
-                        {currency === CURRENCIES.PLN
-                            ? product.pricePLN
-                            : product.priceUSD}{" "}
-                        {currency === CURRENCIES.PLN
-                            ? CURRENCY_SIGN.PLN
-                            : CURRENCY_SIGN.USD}
-                    </p>
-                </div>
-            </div>
-        </>
-    );
+        <div>
+          <p className={styles.name}>{product.productName}</p>
+          <p className={styles.productPrice}>
+            {currency === CURRENCIES.PLN ? product.pricePLN : product.priceUSD}{" "}
+            {currency === CURRENCIES.PLN
+              ? CURRENCY_SIGN.PLN
+              : CURRENCY_SIGN.USD}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
