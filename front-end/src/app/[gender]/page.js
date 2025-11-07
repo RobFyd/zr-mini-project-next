687a82e4af2613d8e10fe2cd252de24ee165e228
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-11-07 00:17:04 +0000

    feat: pass gender parameter to MainPage component in GenderMainPage

--- front-end/src/app/[gender]/page.js
+++ front-end/src/app/[gender]/page.js
@@ -1,5 +1,5 @@
 import { MainPage } from "../views/MainPage/MainPage";
 
-export default function GenderMainPage() {
-  return <MainPage />;
+export default function GenderMainPage({ params }) {
+  return <MainPage gender={params.gender} />;
 }
